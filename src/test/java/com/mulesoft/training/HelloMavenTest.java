c7b59586ededbe98a6b350acc29b6f76e5b56ea5
alars84 <alars84@gmail.com>
2017-07-20 09:33:15 -0500

    added test case 2 to check header property content type

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -25,5 +26,12 @@ public class HelloMavenTest extends FunctionalTestCase {
     	return files;
     }
     
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	System.out.println("\n\nDynamic Port in Test case-2: " + dynamicPort.getNumber() + "\n\n");
+        MuleEvent event = runFlow("retrieveFlights");
+        String contentType = event.getMessage().getOutboundProperty("Content-Type");
+        assertEquals("application/json", contentType);
+    }
 
 }
